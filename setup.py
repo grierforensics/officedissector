dc2f138b72912e87d17695f9a6b93574782aa53b
GrierForensics <dovid@telecurve.com>
2015-04-19 03:07:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,6 @@ setup(
     author_email='jdgrier@grierforensics.com',
     description="""OfficeDissector is a parser library for static security analysis of OOXML documents.""",
     name='officedissector',
+    version='0.0.2',
+    url='https://github.com/grierforensics/officedissector',
     packages=['officedissector'])
